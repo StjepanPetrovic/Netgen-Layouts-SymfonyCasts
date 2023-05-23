247bf81cdd2b58e57735674302bacc0d095aea37
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-05-23 02:43:50 +0200

    Add searching feature during dynamic selection of recipes with Layouts

--- src/Layouts/LatestRecipeQueryTypeHandler.php
+++ src/Layouts/LatestRecipeQueryTypeHandler.php
@@ -6,6 +6,7 @@ use App\Repository\RecipeRepository;
 use Netgen\Layouts\API\Values\Collection\Query;
 use Netgen\Layouts\Collection\QueryType\QueryTypeHandlerInterface;
 use Netgen\Layouts\Parameters\ParameterBuilderInterface;
+use Netgen\Layouts\Parameters\ParameterType\TextType;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
 #[AutoconfigureTag('netgen_layouts.query_type_handler', ['type' => 'latest_recipes'])]
@@ -17,12 +18,13 @@ class LatestRecipeQueryTypeHandler implements QueryTypeHandlerInterface
     
     public function buildParameters(ParameterBuilderInterface $builder): void
     {
+        $builder->add('term', TextType::class);
     }
 
     public function getValues(Query $query, int $offset = 0, ?int $limit = null): iterable
     {
         return $this->recipeRepository
-            ->createQueryBuilderOrderedByNewest()
+            ->createQueryBuilderOrderedByNewest($query->getParameter('term')->getValue())
             ->setFirstResult($offset)
             ->setMaxResults($limit)
             ->getQuery()
@@ -32,7 +34,7 @@ class LatestRecipeQueryTypeHandler implements QueryTypeHandlerInterface
     public function getCount(Query $query): int
     {
         return $this->recipeRepository
-            ->createQueryBuilderOrderedByNewest()
+            ->createQueryBuilderOrderedByNewest($query->getParameter('term')->getValue())
             ->select('COUNT(recipe.id)')
             ->getQuery()
             ->getSingleScalarResult();
