33b16103a1c1f731e5b781f46340d45fcaa4e8ef
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-05-30 03:34:59 +0200

    Add recipe preview feature in ContentBrowser

--- src/ContentBrowser/RecipeBrowserBackend.php
+++ src/ContentBrowser/RecipeBrowserBackend.php
@@ -32,7 +32,7 @@ class RecipeBrowserBackend implements BackendInterface
 
     public function loadItem($value): ItemInterface
     {
-        // TO DO
+        return new RecipeBrowserItem($this->recipeRepository->find($value));
     }
 
     public function getSubLocations(LocationInterface $location): iterable
